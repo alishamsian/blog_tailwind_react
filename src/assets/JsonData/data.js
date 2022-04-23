157c0709f7a6dde95638912bcb26cb9331251421
Ali Shamsian <Alishamsian504@gmail.com>
2022-04-23 19:46:10 +0430

    end create blog

---
+++ src/assets/JsonData/data.js
@@ -0,0 +1,71 @@
+
+
+export const heroData = [
+  {
+    id: 1,
+    name: "بستنی",
+    decp: "شکلات و وانیل",
+    price: "5.25",
+    // imageSrc: I1,
+  },
+  {
+    id: 2,
+    name: "توت فرنگی",
+    decp: "توت فرنگی تازه",
+    price: "10.25",
+    // imageSrc: F1,
+  },
+  {
+    id: 3,
+    name: "کباب مرغ",
+    decp: "بشقاب کباب مخلوط",
+    price: "8.25",
+    // imageSrc: C3,
+  },
+  {
+    id: 4,
+    name: "ماهی کباب",
+    decp: "بشقاب ماهی مخلوط",
+    price: "5.25",
+    // imageSrc: Fi1,
+  },
+];
+
+
+export const categories = [
+    {
+        id: 1,
+        name: "برنامه نویسی",
+        urlParamName: "Programming",
+    },
+    {
+        id: 2,
+        name: "رمان",
+        urlParamName: "Novel",
+    },
+    {
+        id: 3,
+        name: "توسعه فردی",
+        urlParamName: "Individual development",
+    },
+    {
+        id: 4,
+        name: "کسب و کار",
+        urlParamName: "Business",
+    },
+    {
+        id: 5,
+        name: "سبک زندگی",
+        urlParamName: "life-style",
+    },
+    {
+        id: 6,
+        name: "آموزشی",
+        urlParamName: "Educational",
+    },
+    {
+        id: 7,
+        name: "علمی و فناوری",
+        urlParamName: "Science-Technology",
+    },
+]
--- src/page/Create.js
+++ src/page/Create.js
@@ -1,101 +1,143 @@
 import { useState } from "react";
 import BannerTop from "../components/BannerTop";
+import {
+  MdCloudUpload,
+  MdDelete,
+  MdFoodBank,
+  MdAttachMoney,
+  MdCreate,
+  MdTitle,
+  MdTextFields,
+} from "react-icons/md";
+import { categories } from "../assets/JsonData/data";
+import Loader from "../components/Loader";
 
 const Create = () => {
-  const [title, setTitle] = useState();
-  const [tag, setTag] = useState();
-  const [body, setBody] = useState();
-  const [author, setAuthor] = useState("ali shamsian");
-  const [ispending, setIspending] = useState(false);
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setIspending(true);
-    const blog = { title, body, author };
-    fetch("http://localhost:8000/blogs", {
-      method: "POST",
-      headers: { "Content-type": "application/json" },
-      body: JSON.stringify(blog),
-    }).then(() => {
-      setIspending(false);
-    });
-  };
+  const [title, setTitle] = useState("");
+  const [subtitle, setSubtitle] = useState("");
+  const [text, setText] = useState("");
+  const [category, setCategory] = useState(null);
+  const [fields, setFields] = useState(false);
+  const [imageAsset, setImageAsset] = useState(null);
+  const [isLoadiing, setIsLoadiing] = useState(false);
 
   return (
     <>
       <BannerTop />
-
-      <div className="container px-4 md:px-0 max-w-6xl mx-auto -mt-32">
-        <div className="mx-0 sm:mx-6">
-          <div className="flex h-full bg-white rounded overflow-hidden shadow-lg">
-            <form
-              className="container text-center py-9 "
-              onSubmit={handleSubmit}
-            >
-              <div className="flex justify-between px-5">
-              <button
-                type="button"
-                className="flex items-center px-6 py-2 border-2 border-gray-800 text-gray-800 font-medium leading-tight uppercase rounded hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out"
-              >
-                افزودن تصویر
-                <svg
-                  aria-hidden="true"
-                  focusable="false"
-                  data-prefix="fas"
-                  data-icon="download"
-                  className="w-4 mr-2"
-                  role="img"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 512 512"
-                >
-                  <path
-                    fill="currentColor"
-                    d="M216 0h80c13.3 0 24 10.7 24 24v168h87.7c17.8 0 26.7 21.5 14.1 34.1L269.7 378.3c-7.5 7.5-19.8 7.5-27.3 0L90.1 226.1c-12.6-12.6-3.7-34.1 14.1-34.1H192V24c0-13.3 10.7-24 24-24zm296 376v112c0 13.3-10.7 24-24 24H24c-13.3 0-24-10.7-24-24V376c0-13.3 10.7-24 24-24h146.7l49 49c20.1 20.1 52.5 20.1 72.6 0l49-49H488c13.3 0 24 10.7 24 24zm-124 88c0-11-9-20-20-20s-20 9-20 20 9 20 20 20 20-9 20-20zm64 0c0-11-9-20-20-20s-20 9-20 20 9 20 20 20 20-9 20-20z"
-                  ></path>
-                </svg>
-              </button>
-              <input
-                className=" px-3  my-0 border-b-2 border-gray-800 text-slate-800 bg-transparent box-border block focus:outline-none "
-                placeholder="+ حداکثر 5 تگ اضافه کنید"
-                type="text"
-                value={tag}
-                onChange={(e) => setTag(e.target.value)}
-              />
+      <div className="container px-4 md:px-0 max-w-4xl mx-auto -mt-32 flex h-full w-full bg-white rounded overflow-hidden">
+              <div className="w-full rounded-lg p-4 flex flex-col items-center justify-center  gap-8">
+                {fields && (
+                  <p className="w-full p-2 rounded-lg text-center text-lg font-semibold"></p>
+                )}
+                <div className="w-full py-4 border-b border-gray-300 flex items-center gap-2 ">
+                  <MdTitle className="text-xl text-gray-700" />
+                  <input
+                    type="text"
+                    required
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                    placeholder="عنوان پست ..."
+                    className="w-full h-full text-lg bg-transparent outline-none border-none placeholder:text-gray-400 text-textColor"
+                  />
+                </div>
+                <div className="w-full py-4 border-b border-gray-300 flex items-center gap-2">
+                    <MdTextFields className="text-gray-700 text-2xl" />
+                    <input
+                      type="text"
+                      required
+                      value={subtitle}
+                      onChange={(e) => setSubtitle(e.target.value)}
+                      placeholder="پاورقی  ..."
+                      className="w-full h-full text-lg bg-transparent outline-none border-none placeholder:text-gray-400 text-textColor"
+                    />
+                  </div>
+                
+                <div className="w-full flex flex-col md:flex-row items-center gap-3">
+                  <div className="w-full flex flex-col md:flow-row items-center gap-3">
+                    <div className="w-full py-4 border-b border-gray-300 flex items-center gap-2">
+                      <MdCreate className="text-gray-700 text-2xl" />
+                      <input
+                        type="text"
+                        required
+                        value={text}
+                        onChange={(e) => setText(e.target.value)}
+                        placeholder="داستانت را بنویس ..."
+                        className="w-full h-full text-lg bg-transparent outline-none border-none placeholder:text-gray-400 text-textColor"
+                      />
+                    </div>
+                  </div>
+                </div>
+                <div className="w-full">
+                  <select
+                    onChange={(e) => setCategory(e.target.value)}
+                    className="outline-none w-full text-base border-b-2 border-gray-200 p-2 rounded-md cursor-pointer"
+                  >
+                    <option value="other" className="bg-white">
+                      انتخاب دسته بندی
+                    </option>
+                    {categories.map((item) => (
+                      <option
+                        key={item.id}
+                        className="text-base border-0 outline-none capitalize bg-white text-headingColor"
+                        value={item.urlParamName}
+                      >
+                        {item.name}
+                      </option>
+                    ))}
+                  </select>
+                </div>
+                <div className="group flex justify-center items-center flex-col border-2 border-dotted border-gray-300 w-full h-225 md:h-420 cursor-pointer rounded-lg ">
+                  {isLoadiing ? (
+                    <Loader />
+                  ) : (
+                    <>
+                      {!imageAsset ? (
+                        <>
+                          <label className="w-full h-full flex flex-col items-center justify-center cursor-pointer">
+                            <div className="w-full h-full flex flex-col items-center justify-center gap-2">
+                              <MdCloudUpload className="text-gray-500 text-3xl hover:text-gray-700" />
+                              <p className="text-gray-500 hover:text-gray-700">
+                                برای بارگذاری کلیک کنید
+                              </p>
+                            </div>
+                            <input
+                              type="file"
+                              name="uploadimage"
+                              accept="image/*"
+                              className="w-0 h-0"
+                            />
+                          </label>
+                        </>
+                      ) : (
+                        <>
+                          <div className="relative h-full">
+                            <img
+                              src={imageAsset}
+                              alt="uploaded image"
+                              className="w-full h-full object-cover"
+                            />
+                            <button
+                              type="button"
+                              className="absolute bottom-0 right-0 p-3 rounded-full bg-red-500 text-xl cursor-pointer outline-none hover:shadow-md duration-500 transition-all ease-in-out"
+                            >
+                              <MdDelete className="text-white" />
+                            </button>
+                          </div>
+                        </>
+                      )}
+                    </>
+                  )}
+                </div>
+                <div className="flex items-center w-full py-2">
+                  <button
+                    type="button"
+                    className="ml-0 md:m-auto w-full md:w-auto border-none outline-none bg-emerald-500 px-12 py-2 rounded-lg text-lg text-white font-semibold "
+                  >
+                    ذخیره کن
+                  </button>
+                </div>
               </div>
-              
-              <input
-                className="w-full px-6 py-10  my-0 border-none font-bold text-2xl text-slate-800 bg-transparent box-border block focus:outline-none "
-                placeholder="عنوان را اینجا وارد کنید"
-                type="text"
-                value={title}
-                onChange={(e) => setTitle(e.target.value)}
-              />
-              <textarea
-                className="w-full px-6 py-10  my-0  font-bold text-lg text-slate-800 bg-transparent box-border block focus:outline-none "
-                placeholder="هرچی دوست داری بنویس ..."
-                value={body}
-                onChange={(e) => setBody(e.target.value)}
-              />
-              {!ispending && (
-                <button
-                  type="submit"
-                  className="mt-4 md:mt-0 appearance-none bg-green-500 text-white text-base font-semibold tracking-wider uppercase p-4 rounded shadow hover:bg-green-400"
-                >
-                  افزودن پست
-                </button>
-              )}
-              {ispending && (
-                <button
-                  type="submit"
-                  className="mt-4 md:mt-0 appearance-none bg-green-500 text-white text-base font-semibold tracking-wider uppercase p-4 rounded shadow hover:bg-green-400"
-                >
-                  در حال ارسال
-                </button>
-              )}
-            </form>
-          </div>
-        </div>
-      </div>
+            </div>
     </>
   );
 };
